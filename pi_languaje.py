733d707af593c435e5da1a7d9fec89dec8187cd8
Alba de Diego <albadediegocorrea@gmail.com>
2025-10-13 22:02:07 +0200

    primer planteamiento de funciones

--- pi_languaje.py
+++ pi_languaje.py
@@ -1,8 +1,17 @@
+CONSONANTES = ("b", "c", "d", "f", "g", "h", "j", "k", "l", "ll", "m", "n", "ñ", "p", "q", "r", "s", "t", "v", "w", "x", "z")
+VOCALES_ABIERTAS = ("a", "e", "o")
+VOCALES_CERRADAS = ("i", "u")
+SEMIVOCALES = ("y",)
+PARES_CONSONANTES = ("bl", "cl", "fl", "gl", "kl", "pl", "tl","br", "cr", "dr", "fr",
+    "gr", "kr", "pr", "tr","ch", "ll", "rr"
+)
+
+
+def silabear(palabra):
+    pass
+
 def normal_a_pi(palabra):
     pass
 
 def pi_a_normal(palabra):
-    pass
-
-def silabear(palabra):
     pass
